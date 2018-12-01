85802294e3770fb81340cf5d5868156e5adab694
hgedia <hgedia@hgedia.co>
2018-12-01 16:16:26 +0000

    Script to deploy contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,6 +6,10 @@ const FIFSRegistrar = artifacts.require('./FIFSRegistrar.sol');
 // not existed, it's valid to put it here.
 // TODO: align the contract name with the source code file name.
 const Registrar = artifacts.require('./Registrar.sol');
+const DefaultReverseResolver = artifacts.require('./DefaultReverseResolver.sol')
+const ReverseRegistrar = artifacts.require('./ReverseRegistrar.sol')
+const SubdomainRegistrar = artifacts.require('./SubdomainRegistrar.sol')
+const PublicResolver = artifacts.require('./PublicResolver.sol')
 const web3 = new (require('web3'))();
 const namehash = require('eth-ens-namehash');
 
@@ -64,14 +68,70 @@ function deployAuctionRegistrar(deployer, tld) {
     });
 }
 
-module.exports = function(deployer, network) {
-  var tld = 'eth';
+function deployLivepeerENSRegistry(deployer,tld,accounts){
+  let rootNode = getRootNodeFromTLD(tld);
+  
+  //1. Deploy ENS and FIFS registrar
+  deployer.deploy(ENS)
+    .then(() => {
+      console.log("=> Deploying registrar")
+      return deployer.deploy(Registrar, ENS.address, rootNode.namehash, 0)
+    })
+    .then(()=>{
+      console.log("=> Setting TLD owner")
+      return ENS.at(ENS.address).setSubnodeOwner('0x0', rootNode.sha3, Registrar.address);
+    })
+    .then(()=>{
+      console.log("=> Deploying public resolver")
+      return deployer.deploy(PublicResolver,ENS.address);
+    })
+    
 
-  if (network === 'dev.fifs') {
+  //2. Setup Reverse registration
+  deployer.deploy(DefaultReverseResolver,ENS.address)
+  .then(()=>{
+    console.log("=>Deploying Reverse registrar")    
+    return deployer.deploy(ReverseRegistrar, ENS.address, DefaultReverseResolver.address);
+  })
+  .then(()=>{
+   console.log("=> Setting up reverse node")
+   return ENS.at(ENS.address).setSubnodeOwner('0x0', web3.sha3('reverse'), accounts[0]);
+  })
+  .then(()=>{
+    console.log("=> Setting up reverse node owner to registrar")
+    return ENS.at(ENS.address).setSubnodeOwner(namehash('reverse'), web3.sha3('addr'), ReverseRegistrar.address);
+  });
+
+  //3. Setup livepeer contract registry  
+  deployer.deploy(SubdomainRegistrar, ENS.address, PublicResolver.address, namehash("livepeer.eth"), "livepeer.eth")
+  .then(()=> {
+    return ENS.at(ENS.address).setSubnodeOwner(namehash('eth'), web3.sha3('livepeer'), accounts[0])
+  })
+  .then(()=> {
+    return ENS.at(ENS.address).setOwner(namehash('livepeer.eth'), SubdomainRegistrar.address);
+  })
+
+  // Deploy the ENS & setup regitrar
+  console.log("=".repeat(25) + " Deployment status " + "=".repeat(25))
+  console.log("Account " + accounts[0])
+  console.log("ENS : " + ENS.address);
+  console.log("ENS FIFS Registrar : " + Registrar.address);
+  console.log("Reverse Registrar : " + ReverseRegistrar.address);
+  console.log("Default Reverse resolver  : " + DefaultReverseResolver.address);
+  console.log("Livepeer Subdomain Registrar : " + SubdomainRegistrar.address);
+  console.log("=".repeat(72))
+}
+
+
+
+module.exports = function(deployer, network,accounts) {
+  var tld = 'eth';
+  if(network ==='dev.livepeer'){
+    deployLivepeerENSRegistry(deployer,tld,accounts)
+  } else if (network === 'dev.fifs') {
     deployFIFSRegistrar(deployer, tld);
   }
   else if (network === 'dev.auction') {
     deployAuctionRegistrar(deployer, tld);
   }
-
 };
--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+let mnemonic = "celery clarify connect system armor clarify gaze enable junk stock inherit dish debris expire around"
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     'dev.fifs': {
@@ -9,6 +12,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    'dev.livepeer':{
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/YmgC8ZwX9OFD8WS2tV9y")
+      },
+      network_id: "*",
+      gasPrice: 5000000000
     }
   }
 };
